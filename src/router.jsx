16ab892d778e1516e452a6f192cc643a0524ae33
Neph <nephcohen@gmail.com>
2025-03-31 23:58:35 +0200

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// IMPORT LIB ====================================================//
 // IMPORT LAYOUT =================================================//
 import Header from './layout/Header';
 import Footer from './layout/Footer';
