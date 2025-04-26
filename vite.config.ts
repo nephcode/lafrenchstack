1a56acb7855c9e951da1dd40bc8028f538e7a4d8
Neph <nephcohen@gmail.com>
2025-04-26 10:58:55 +0200

    nix config 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  },
 });
 
